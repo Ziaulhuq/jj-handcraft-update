a29e4990ed280068f1e9363f6f9f74bbfb655913
ZIA UL HUQ <ziaulhuq@ZIAs-MacBook-Pro.local>
2026-02-10 23:41:36 +0800

    initial upload

---
+++ components/shared/home/home-card.tsx
@@ -0,0 +1,57 @@
+import Image from "next/image";
+import Link from "next/link";
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
+
+type CardItem = {
+  title: string;
+  link: { text: string; href: string };
+  items: {
+    name: string;
+    items?: string[];
+    image: string;
+    href: string;
+  }[];
+};
+
+export function HomeCard({ cards }: { cards: CardItem[] }) {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 lg:grid-cols-3 md:gap-4">
+      {cards.map((card) => (
+        <Card key={card.title} className="rounded-none flex flex-col">
+          <CardContent className="p-4 flex-1">
+            <h3 className="text-xl font-bold mb-4">{card.title}</h3>
+            <div className="grid grid-cols-2 gap-2">
+              {card.items.map((item) => (
+                <Link
+                  key={item.name}
+                  href={item.href}
+                  className="flex flex-col p-4 gap-2 bg-gray-50"
+                >
+                  <Image
+                    src={item.image}
+                    alt={item.name}
+                    className="w-full h-full"
+                    height={100}
+                    width={120}
+                  />
+                  <p className="text-center text-sm font-semibold whitespace-nowrap overflow-hidden text-ellipsis mt-2">
+                    {item.name}
+                  </p>
+                </Link>
+              ))}
+            </div>
+          </CardContent>
+          {card.link && (
+            <CardFooter>
+              <Link href={card.link.href} className="mt-4 block">
+                <button className="bg-yellow-300 p-2 font-extrabold text-[13px] rounded-lg hover:bg-yellow-400">
+                  {card.link.text}
+                </button>
+              </Link>
+            </CardFooter>
+          )}
+        </Card>
+      ))}
+    </div>
+  );
+}
---
+++ components/shared/home/home-carousel.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import * as React from "react";
+import Image from "next/image";
+import Autoplay from "embla-carousel-autoplay";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+
+export function HomeCarousel({
+  items,
+}: {
+  items: {
+    image: string;
+    url: string;
+    title: string;
+    buttonCaption: string;
+  }[];
+}) {
+  const plugin = React.useRef(
+    Autoplay({ delay: 3000, stopOnInteraction: true })
+  );
+
+  return (
+    <Carousel
+      dir="ltr"
+      plugins={[plugin.current]}
+      className="w-full mx-auto "
+      onMouseEnter={plugin.current.stop}
+      onMouseLeave={plugin.current.reset}
+    >
+      <CarouselContent>
+        {items.map((item) => (
+          <CarouselItem key={item.title}>
+            <Link href={item.url}>
+              <div className="flex aspect-18/8 items-center justify-center p-6 relative -m-1">
+                <Image
+                  src={item.image}
+                  alt={item.title}
+                  fill
+                  className="object-cover"
+                  priority
+                />
+                <div className="absolute w-1/3 left-16 md:left-32 top-1/2 transform -translate-y-1/2">
+                  <h2
+                    className={cn(
+                      "text-xl md:text-5xl sm:text-3xl font-extrabold text-primary mb-2"
+                    )}
+                  >
+                    {item.title}
+                  </h2>
+                  <Button className="mt-8 hidden md:block">
+                    {item.buttonCaption}
+                  </Button>
+                </div>
+              </div>
+            </Link>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious className="left-0 md:left-12 bg-amber-100 text-black" />
+      <CarouselNext className="right-0 md:right-12 text-black bg-amber-100" />
+    </Carousel>
+  );
+}
